a95f6931d3579325552d1404e6a4aeec632e0647
chen-chenglong <chenglongchen@qq.com>
2026-05-20 11:39:47 +0800

    1

--- src/config/sponsorConfig.ts
+++ src/config/sponsorConfig.ts
@@ -39,22 +39,22 @@ export const sponsorConfig: SponsorConfig = {
 			description: "使用 微信 扫码赞助",
 			enabled: true,
 		},
-		// {
-		// 	name: "ko-fi",
-		// 	icon: "simple-icons:kofi",
-		// 	qrCode: "",
-		// 	link: "https://ko-fi.com/cuteleaf",
-		// 	description: "Buy a Coffee for Firefly",
-		// 	enabled: true,
-		// },
-		// {
-		// 	name: "爱发电",
-		// 	icon: "simple-icons:afdian",
-		// 	qrCode: "",
-		// 	link: "https://ifdian.net/a/cuteleaf",
-		// 	description: "通过 爱发电 进行赞助",
-		// 	enabled: true,
-		// },
+		{
+		 	name: "ko-fi",
+		 	icon: "simple-icons:kofi",
+		 	qrCode: "",
+		 	link: "https://ko-fi.com/cuteleaf",
+		 	description: "Buy a Coffee for Firefly",
+		 	enabled: true,
+		},
+		{
+		 	name: "爱发电",
+		 	icon: "simple-icons:afdian",
+		 	qrCode: "",
+		 	link: "https://ifdian.net/a/cuteleaf",
+		 	description: "通过 爱发电 进行赞助",
+		 	enabled: true,
+		},
 	],
 
 	// 赞助者列表（可选）
@@ -70,7 +70,7 @@ export const sponsorConfig: SponsorConfig = {
 		{
 			name: "潘军",
 			amount: "¥66",
-			date: "2026-05-16",
+			date: "2026-05-01",
 		},
 	],
 };
