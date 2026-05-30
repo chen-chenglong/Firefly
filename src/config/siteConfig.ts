d72f51005e89b56d12108b5ed40eb73623aa2fa7
chen-chenglong <chenglongchen@qq.com>
2026-05-30 16:10:26 +0800

    logo

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -56,7 +56,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "logo.webp",
+			src: "/favicon/logo.webp",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
