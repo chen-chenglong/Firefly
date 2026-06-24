54fcabf0a62058b4fcf0dc997407429f51ae336c
chen-chenglong <chenglongchen@qq.com>
2026-06-24 14:02:42 +0800

    换回busuanzi

--- src/config/analyticsConfig.ts
+++ src/config/analyticsConfig.ts
@@ -34,11 +34,11 @@ export const analyticsConfig: AnalyticsConfig = {
 	// 51la 统计配置
 	la51Analytics: {
 		// 51la 统计 ID
-		Id: "3Q5qajWb3P3Css8u",
+		Id: "",
 		// 自定义 SDK JS 地址，防止 DNS 污染，留空使用默认地址
 		sdkUrl: "",
 		// 多个统计 ID 的数据分离标识，留空则使用 Id
-		ck: "3Q5qajWb3P3Css8u",
+		ck: "",
 		// 是否开启事件分析功能
 		autoTrack: false,
 		//  Hash路由模式, 项目使用History API路由, 所以不必开启默认false
