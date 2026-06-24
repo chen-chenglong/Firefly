0679ff41648191a514a3dd74c3d2d1a7b591d5fa
chen-chenglong <chenglongchen@qq.com>
2026-06-24 18:03:53 +0800

    目录固定

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -126,6 +126,18 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			},
 		},
 		{
+			// 组件类型：侧边栏目录组件（只在文章详情页显示）
+			type: "sidebarToc",
+			// 是否启用该组件
+			enable: true,
+			// 组件位置
+			position: "sticky",
+			// 是否在文章详情页显示
+			showOnPostPage: true,
+			// 是否在非文章详情页隐藏
+			hideOnNonPostPage: true,
+		},
+		{
 			// 组件类型：日历组件
 			type: "calendar",
 			// 是否启用该组件
@@ -143,18 +155,6 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 				showHeatmap: true,
 			},
 			},
-		},
-		{
-			// 组件类型：侧边栏目录组件（只在文章详情页显示）
-			type: "sidebarToc",
-			// 是否启用该组件
-			enable: true,
-			// 组件位置
-			position: "top",
-			// 是否在文章详情页显示
-			showOnPostPage: true,
-			// 是否在非文章详情页隐藏
-			hideOnNonPostPage: true,
 		},
 		{
 			// 组件类型：广告栏组件 1
