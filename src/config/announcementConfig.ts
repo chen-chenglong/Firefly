a1ec44578ec4bc1fec566678b571c94f7da7e29d
chen-chenglong <chenglongchen@qq.com>
2026-08-10 16:40:34 +0800

    66688

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -5,7 +5,7 @@ export const announcementConfig: AnnouncementConfig = {
 	title: "公告",
 
 	// 公告内容
-	content: "欢迎来到我的博客！这是一则示例公告。",
+	content: "欢迎来到旷野星火，每一个字的首字母构成了本站的域名：kyxh.cc。",
 
 	// 是否允许用户关闭公告
 	closable: false,
