9660c3c40d593d8e180ec21f492b649b076c88de
chen-chenglong <chenglongchen@qq.com>
2026-08-11 08:52:09 +0800

    logo

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,7 +6,7 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "assets/images/avatar.avif",
+	avatar: "assets/images/logoKYXH.webp",
 
 	// 名字
 	name: "旷野星火",
@@ -24,7 +24,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "Email",
 			icon: "fa7-solid:envelope",
-			url: "651080920@qq.com",
+			url: "mailto:651080920@qq.com",
 			showName: false,
 		},
 		{
